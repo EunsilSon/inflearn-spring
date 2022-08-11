de4be8330612fd9b62c3dc1dee450351e9ea13fe
EunsilSon <eunsil1023@gmail.com>
2022-08-12 00:26:25 +0900

    2022-08-11 / 스프링 빈과 의존 관계
    1. 컴포넌트 스캔과 자동 의존관계 설정
    - @Controller 스프링이 자동으로 객체를 생성해 스프링 컨테이너에서 스프링 Bean으로 등록 및 관리 => 싱글톤
    - @Autowired 스프링 컨테이너에서 자동으로 의존관계를 설정 => DI (Dependency Injection: 의존성 주입)
        스프링 컨테이너에 올라간 것만 @Autowired 동작함
    
    * @Controller, @Service, @Repository 내부에 @Component가 있음 => 스프링이 객체로 생성해 스프링 컨테이너에 등록
    * 패키지 단위로 컴포넌트 스캔 수행
    
    2. 자바 코드로 직접 스프링 빈 등록하기
    - @Bean 스프링 Bean 등록

--- src/main/java/com/inflearn/hellospring/service/MemberService.java
+++ src/main/java/com/inflearn/hellospring/service/MemberService.java
@@ -3,16 +3,20 @@ package com.inflearn.hellospring.service;
 import com.inflearn.hellospring.domain.Member;
 import com.inflearn.hellospring.repository.MemberRepository;
 import com.inflearn.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+//@Service
 public class MemberService {
 
-    private final MemberRepository memoryMemberRepository;
+    private final MemberRepository memberRepository;
 
-    public MemberService(MemoryMemberRepository memoryMemberRepository) {
-        this.memoryMemberRepository = memoryMemberRepository;
+    //@Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     /*
@@ -21,12 +25,12 @@ public class MemberService {
     public Long join(Member member) {
         // ctrl + alt + m : 메소드 자동 생성
         validateDuplicateMember(member); // 중복 체크
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        memoryMemberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -36,10 +40,10 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memoryMemberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-        return memoryMemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
